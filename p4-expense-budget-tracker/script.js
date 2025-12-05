884e3d73f27a6a87d2f1923e7b81bcc41d2d6ebb
Cesar <cfitchf@wgu.edu>
2025-12-04 17:01:00 -0800

    Add local storage Add totalSpent();
    The transactions are stored and the total spent will be display as well as the current balance.

--- p4-expense-budget-tracker/script.js
+++ p4-expense-budget-tracker/script.js
@@ -4,6 +4,7 @@ const amountEl = document.getElementById("amount");
 const type = document.getElementById("type");
 const form = document.getElementById("transaction-form");
 const transactionList = document.getElementById("transaction-list");
+const spent = document.getElementById("spent");
 
 let transactionArray = [];
 
@@ -36,12 +37,16 @@ function displayExpenses(name, amount, category) {
 
     // Store object in array
     transactionArray.push({ id: numId, text: name, amount: Number(amount), type: category });
+    
+    savedItems();
 
     // Clear values
     text.value = "";
     amountEl.value = "";
 
     updateBalance();
+        
+    totalSpent();
 
     // Deletes item
     newBtn.addEventListener("click", (event) => {
@@ -50,12 +55,11 @@ function displayExpenses(name, amount, category) {
         
         deleteTransaction(parentElement.id);
     });
-
 };
 
 function updateBalance() {
     const sum = transactionArray.reduce((accumulator, currentValue) => {
-        if (currentValue.type == "expense") {
+        if (currentValue.type === "expense") {
             return accumulator - currentValue.amount;
         }
         else {
@@ -67,19 +71,84 @@ function updateBalance() {
     balance.textContent = `$${sum.toFixed(2)}`;
 }
 
+function totalSpent() {
+    // Total spent
+    const totalSpent = transactionArray.reduce((acc, item) => {
+        return item.type === "expense" ? acc + item.amount : acc;
+    }, 0);
+
+    if (transactionArray.length === 0) {
+        spent.textContent = `Total Spent: `;
+    } else {
+        spent.textContent = `Total Spent: ${totalSpent.toFixed(2)}`;
+    }
+}
+
 function deleteTransaction(id) {
     const targetedLi = document.getElementById(id);
     targetedLi.remove();
 
-    // If object id is not the same as list id it keep in array
+    // If object id is not the same as list id it keeps in array
     transactionArray = transactionArray.filter(item => item.id !== Number(id));
 
+    savedItems();
+    updateBalance();
+    totalSpent();
+}
+
+function addTransactionToUI(transaction) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const btn = document.createElement("button");
+
+    li.id = transaction.id;
+    li.classList.add("transaction");
+
+    if(transaction.type === "income") {
+        li.classList.add("income");
+    } else {
+        li.classList.add("expense");
+    }
+
+    span.textContent = `${transaction.text} - $${parseFloat(transaction.amount).toFixed(2)}`;
+    btn.innerHTML = `❌`;    
+    btn.classList.add("delete-btn");
+
+    li.appendChild(span);
+    li.appendChild(btn);
+    transactionList.appendChild(li);
+
+    // Add delete listener
+    btn.addEventListener("click", () => {
+        deleteTransaction(transaction.id);
+    })
+
+
+    totalSpent();
     updateBalance();
 }
+
+
+function savedItems() {
+    localStorage.setItem("transactions", JSON.stringify(transactionArray));
+}
+
+function loadItems() {
+    const saved = JSON.parse(localStorage.getItem("transactions")) || [];
+    if (!saved) return;
+
+    transactionArray = saved;
+;
+    saved.forEach(item => {
+        addTransactionToUI(item);
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", loadItems);
 
 // Event listeners
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     displayExpenses(text.value, amountEl.value, type.value);
 });
-
